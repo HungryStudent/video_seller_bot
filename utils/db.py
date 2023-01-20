1d686ad2f70d4c1c1c1107c298cb33a25d625f03
HungryStudent <nikita.efan.2016@mail.ru>
2023-01-20 16:51:36 +0300

    db fix

--- utils/db.py
+++ utils/db.py
@@ -5,7 +5,7 @@ import sqlite3
 database = "utils/database.db"
 
 
-def dict_factory(cursor, rowq):
+def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
